c1401aee6337daae89638e396d865a203a28b66c
George Santos <47012804+myur4@users.noreply.github.com>
2022-05-16 07:31:54 -0700

    Minimal changes, syntax fix in Client

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import SelenaClient from './SelenaClient';
 
 dotenv.config();
 
-// eslint-disable-next-line no-unused-vars
-const client: SelenaClient = new SelenaClient({
+const connection = new SelenaClient({
   administrators: ['263477867608801280'],
   token: process.env.TOKEN as string,
 });
+
+export default connection;
--- src/interfaces/Container.ts
+++ src/interfaces/Container.ts
@@ -1,3 +1,4 @@
+import { GuildMember } from 'discord.js';
 import SelenaClient from '../SelenaClient';
 import { SGuild } from './Guild';
 import IModerator from './Moderator';
@@ -5,6 +6,8 @@ import IModerator from './Moderator';
 interface Container {
     client: SelenaClient
     guildData: SGuild
+    args: string[]
+    member: GuildMember
     moderatorData?: IModerator
 }
 
